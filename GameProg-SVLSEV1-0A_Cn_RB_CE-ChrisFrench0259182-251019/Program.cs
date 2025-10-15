f138fb78be2825103de82e821d483d34550e22a9
CF8339816 <chris_french_3dga@hotmail.com>
2025-10-15 15:31:53 -0300

    Initial commit

---
+++ GameProg-SVLSEV1-0A_Cn_RB_CE-ChrisFrench0259182-251019/Program.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace GameProg_SVLSEV1_0A_Cn_RB_CE_ChrisFrench0259182_251019
+// aka : C# Challenge: Super Virus Land Simulation Extreme V1.0 Alpha Codename Red Blobs (((Collector's Edition)))
+{
+    internal class Program
+    {
+        //variables
+
+       
+
+
+        static string[,] grounds = {
+             
+                { "-" ,"-" ,"~" ,"~" ,"-" ,"-" ,"-" ,"-","-" ,"-" ,"-" ,"-" ,"-" ,"-" ,"-"  },
+                { "-" ,"-" ,"~" ,"~" ,"-" ,"-" ,"-" ,"-","-" ,"-" ,"^" ,"-" ,"-" ,"-" ,"-"  },
+                { "-" ,"-" ,"~" ,"~" ,"-" ,"-" ,"-" ,"-","-" ,"^" ,"^" ,"^" ,"-" ,"-" ,"-"  },
+                { "-" ,"~" ,"~" ,"-" ,"-" ,"-" ,"-" ,"-","-" ,"-" ,"^" ,"-" ,"-" ,"-" ,"-"  },
+                { "-" ,"~" ,"~" ,"-" ,"-" ,"-" ,"-" ,"-","-" ,"-" ,"^" ,"-" ,"-" ,"-" ,"-"  },
+                { "~" ,"~" ,"-" ,"-" ,"-" ,"-" ,"-" ,"-","-" ,"^" ,"-" ,"-" ,"-" ,"-" ,"-"  },
+                { "~" ,"~" ,"-" ,"-" ,"-" ,"-" ,"-" ,"-","-" ,"^" ,"^" ,"^" ,"-" ,"-" ,"-"  },
+                { "-" ,"~" ,"~" ,"-" ,"-" ,"-" ,"-" ,"-","-" ,"-" ,"-" ,"^" ,"-" ,"-" ,"-"  },
+                { "-" ,"-" ,"~" ,"~" ,"-" ,"-" ,"-" ,"-","-" ,"-" ,"^" ,"-" ,"-" ,"-" ,"-"  },
+                { "-" ,"-" ,"~" ,"~" ,"-" ,"-" ,"-" ,"-","-" ,"-" ,"-" ,"-" ,"-" ,"-" ,"-"  },
+                { "-" ,"-" ,"~" ,"~" ,"-" ,"-" ,"-" ,"-","-" ,"-" ,"^" ,"-" ,"-" ,"-" ,"-"  },
+                { "-" ,"-" ,"~" ,"-" ,"-" ,"-" ,"-" ,"-","-" ,"-" ,"^" ,"-" ,"-" ,"-" ,"-"  },
+                { "-" ,"~" ,"~" ,"~" ,"~" ,"-" ,"-" ,"-","-" ,"^" ,"^" ,"-" ,"-" ,"-" ,"-"  },
+                { "-" ,"~" ,"~" ,"~" ,"~" ,"~" ,"-" ,"-","-" ,"^" ,"^","-" ,"-" ,"-" ,"-"  },
+                { "-" ,"~" ,"~" ,"~" ,"~" ,"~" ,"-" ,"-","-" ,"^" ,"^" ,"-" ,"-" ,"-" ,"-"  },
+                { "-" ,"-" ,"~" ,"~" ,"~" ,"~" ,"-" ,"-","-" ,"-" ,"-" ,"-" ,"-" ,"-" ,"-"  },
+                { "-" ,"-" ,"~" ,"~" ,"~" ,"-" ,"-" ,"-","-" ,"-" ,"-" ,"-" ,"-" ,"-" ,"-"  },
+                { "-" ,"-" ,"-" ,"-" ,"-" ,"-" ,"-" ,"-","-" ,"-" ,"-" ,"-" ,"-" ,"-" ,"-"  },
+                { "-" ,"-" ,"-" ,"-" ,"-" ,"-" ,"-" ,"-","-" ,"-" ,"-" ,"-" ,"-" ,"-" ,"-"  },
+           
+            };
+
+        // static string grass = "-";
+        //static string hill = "^";
+        // static string water = "~";
+        static string Virus;
+
+
+
+        static void Main(string[] args)
+        {
+
+
+
+
+
+            for (int x = 0; x < grounds.GetLength(0); x++) // Rows
+            {
+                for (int y = 0; y < grounds.GetLength(1); y++) // Columns
+                {
+                    // attempting to set foreground colours  for values when printing the map
+                    //foreach(string ground in grounds)
+
+                    //if (ground == grass)
+                    //    {
+                    //        Console.ForegroundColor = ConsoleColor.Green;
+                    //    }
+                    //else if (ground == hill)
+                    //{
+                    //    Console.ForegroundColor = ConsoleColor.Gray;
+                    //}
+                    //else if (ground == water)
+                    //{
+                    //    Console.ForegroundColor = ConsoleColor.Blue;
+                    //}
+                    //else
+                    //{
+                    //    Console.ForegroundColor = ConsoleColor.White;
+                    //}
+
+
+
+
+                    Console.Write(grounds[x, y] + " ");
+                }
+                Console.WriteLine();
+            }
+
+
+           int startIndex = 0, 6;
+            string newValue = "X";
+
+          
+            grounds[startIndex] = newValue;
+
+       
+            for (int x = 0; x < grounds.GetLength(0); x++) 
+            {
+                for (int y = 0; y < grounds.GetLength(1); y++) 
+                {
+
+                    Console.Write(grounds[x, y] + " ");
+                    
+                }
+            }
+        }
+
+
+
+
+        //methods Below
+        //m1
+
+        //m2
+
+
+        //m3
+
+
+        //m4
+
+
+        //m5
+
+
+        //m6
+
+
+        //m7
+
+
+
+
+
+
+
+    }
+}
---
+++ GameProg-SVLSEV1-0A_Cn_RB_CE-ChrisFrench0259182-251019/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("GameProg-SVLSEV1-0A_Cn_RB_CE-ChrisFrench0259182-251019")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("NSCC")]
+[assembly: AssemblyProduct("GameProg-SVLSEV1-0A_Cn_RB_CE-ChrisFrench0259182-251019")]
+[assembly: AssemblyCopyright("Copyright © NSCC 2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("4ee3dcc9-e7e9-4a31-be80-bb8a12822bc9")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
